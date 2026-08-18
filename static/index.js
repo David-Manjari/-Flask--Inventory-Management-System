ffe09c5c1e7566eca5336161e8f86dd1c7709f9c
David-Manjari <davidmanjari@gmail.com>
2026-08-18 08:27:41 +0300

    Updated path to script file

--- static/index.js
+++ static/index.js
@@ -1,15 +1,22 @@
 // create a method to fetch data from the api
-function GetApiData(){
+fetch("/inventory")
+.then(res=>res.json())
+.then(data=>{
+	data.forEach(item=>{
+    RenderProduct(item)
+    })
+})
 
-}
 
-const name = document.getElementById("name");
-const brand = document.getElementById("brand");
-const ingredients = document.getElementById("ingredients")
+
 const form = document.getElementById("Form")
 
-form.addEventListener("submit", (event) =>{
+form.addEventListener("submit",async (event) =>{
     event.preventDefault();
+
+    const name = document.getElementById("name");
+    const brand = document.getElementById("brand");
+    const ingredients = document.getElementById("ingredients")
     if (name.value ===""){
         alert("Fill the product name field")
         return;
@@ -23,40 +30,58 @@ form.addEventListener("submit", (event) =>{
         alert("Add the ingredients")
         return;
     }
-       
+
 
     const product = {product_name: name.value,
                      brands: brand.value,
-                    ingredients_text: ingredients.value}
+                    ingredients_text: ingredients.value
+                }
     
-// create a methid to add the information to the backend
-        const ressponse = await fetch("/inventory",{
+// create a method to add the information to the backend
+        const response = await fetch("/inventory",{
             method: "POST",
             headers: {
-                "Content-Type": "applicatio/json"
+                "Content-Type": "application/json"
             },
             body: JSON.stringify(product)
         });
 
         // await response from backend
-        const response= await response.JSON()
-    RenderProduct(product)
+        const feedback = await response.json()
+        console.log(feedback.id)
+    RenderProduct(feedback)
 })
 
 function RenderProduct(product){
     const RenderList = document.getElementById('view_list')
-    const newlist = document.createElement("ul")
+    const newList = document.createElement("ul")
     const name = document.createElement('li')
     const brand = document.createElement("li")
     const ingredient = document.createElement("li")
     const details = document.createElement("ul")
-    brand.textContent = "Brand: " + product["brand"]
-    ingredient.textContent = "Ingredients: " + product["ingredients"]
+    brand.textContent = "Brand: " + product["brands"]
+    ingredient.textContent = "Ingredients: " + product['ingredients_text']
     name.textContent = product['product_name']
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+
+    deleteButton.addEventListener("click", async () => {
+        const response = await fetch(`/inventory/${product.id}`, {
+            method: "DELETE"
+        });
+
+        if (response.ok) {
+            newList.remove();
+        }
+        else
+            alert("Could not delete Product")
+    });
     
     details.appendChild(brand)
     details.appendChild(ingredient)
-    newlist.appendChild(name)
-    newlist.appendChild(details)
-    RenderList.appendChild(newlist)
+    newList.appendChild(name)
+    newList.appendChild(details)
+    newList.appendChild(deleteButton)
+    RenderList.appendChild(newList)
 }   
